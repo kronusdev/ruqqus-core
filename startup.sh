273a7eaba0267544e7b930373e66c54e35e1b180
arkit3kt <exodust@protonmail.com>
2021-08-18 16:17:43 -0400

    startup

--- startup.sh
+++ startup.sh
@@ -4,9 +4,11 @@ cd ~/app/ruqqus-core
 #git pull
 sudo cp nginx.txt /etc/nginx/sites-available/ruqqus.com.conf
 sudo nginx -s reload
+#python3.7 -m venv venv
+cd ~
 source venv/bin/activate
-source ~/env.sh
-
+source env.sh
+cd ~/app/ruqqus-core
 pip3 install -r requirements.txt
 export PYTHONPATH=$PYTHONPATH:~/app/ruqqus-core
 export S3_BUCKET_NAME=i.ruqqus.com
@@ -15,7 +17,7 @@ export HCAPTCHA_SITEKEY="22beca86-6e93-421c-8510-f07c6914dadb"
 cd ~/app/ruqqus-core
 
 echo "Starting Background Worker"
-python files/scripts/recomputes.py
+python3.7 files/scripts/recomputes.py
 
 echo "Starting Regular Workers"
 NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program gunicorn files.__main__:app -k gevent -w $WEB_CONCURRENCY --worker-connections $WORKER_CONNECTIONS --preload --max-requests 10000 --max-requests-jitter 500 --bind 127.0.0.1:5000

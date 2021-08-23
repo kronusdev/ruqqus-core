92875b0c33dc17ce84edd4b9ed720af63ce5213b
fireworks88 <throwaway.546372819@protonmail.com>
2021-08-23 23:30:33 +0200

    /

--- files/routes/settings.py
+++ files/routes/settings.py
@@ -13,8 +13,8 @@ valid_username_regex = re.compile("^[a-zA-Z0-9_\-]{3,25}$")
 valid_title_regex = re.compile("^((?!<).){3,100}$")
 valid_password_regex = re.compile("^.{8,100}$")
 
-YOUTUBE_KEY = environ.get("YOUTUBE_KEY").strip()
-COINS_NAME = environ.get("COINS_NAME").strip()
+YOUTUBE_KEY = environ.get("YOUTUBE_KEY", "").strip()
+COINS_NAME = environ.get("COINS_NAME", "rep").strip()
 
 @app.post("/settings/profile")
 @auth_required
